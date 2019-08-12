1f7b5ecfe1891618375e0fcd743ea44c1e0d0c8a
shub1nk <shub1nk@yandex.ru>
2019-08-12 23:21:22 +0500

    correct layout game

--- src/App.js
+++ src/App.js
@@ -6,38 +6,67 @@ class App extends Component {
     super(props);
     this.state = {
       isPlayGame: false,
-      whoseMove: 'user' //TODO: рандомно определять 1 и 0
+      whoseMove: 'user', //TODO: рандомно определять 1 и 0
+      userName: '',
+      compName: ''
 
     }
   }
 
-  handleGame = (state) => {
-    this.setState({isPlayGame: state});
+  handleGame = (state) => this.setState({isPlayGame: state});
+
+  setNamePlayer = (key, ev) => {
+    console.log(key)
+    this.setState({[key]: ev.target.value})
   }
 
   render() {
 
-    const { isPlayGame } = this.state;
+    const { isPlayGame, userName, compName } = this.state;
+
+    console.log("this.state", this.state)
 
     return (
       <section className="b-game">
         <h3 className="b-game__title">Морской бой</h3>
         <div className="b-game__fields">
           <div className="b-game__field__user">
+            {
+              !isPlayGame 
+              ? (<input value={userName} onChange={this.setNamePlayer.bind(this, "userName")}></input>)
+              : (<span className="b-game__field__user__name">{userName}</span>)
+            }
             <Field/>
           </div>
           <div className="b-game__field__comp">
+            {
+              !isPlayGame 
+              ? (<input value={compName} onChange={this.setNamePlayer.bind(this, "compName")}></input>)
+              : (<span  className="b-game__field__comp__name">{compName}</span>)
+            }
             <Field/>
           </div>
         </div>
         {!isPlayGame 
-          ? (<button className="b-game__button_play" onClick={this.handleGame.bind(this, true)}>Играть</button>)
-          : (
-          <Fragment>
-            <button className="b-game__button_reset" onClick={this.handleGame.bind(this, false)}>Сбросить</button>
-            <div className="b-game__log">{`Ходит игрок: ${this.state.whoseMove}`}</div>
-          </Fragment>)
+          ? (<button 
+              className="b-game__button_play" 
+              onClick={this.handleGame.bind(this, true)}
+              disabled={!userName && !compName}
+            >
+              Играть
+            </button>)
+          : (<button 
+              className="b-game__button_reset" 
+              onClick={this.handleGame.bind(this, false)}
+            >
+              Сбросить
+            </button>)
         }
+        <div className="b-game__log">
+          {!isPlayGame 
+            ? 'Для начала игры, введите имена игроков!'
+            : `Ходит игрок: ${this.state.whoseMove}`}
+        </div>
       </section>)
   }
 }
